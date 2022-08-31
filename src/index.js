e4ade4d34b0a8e3c0d8b8eef11c8072fe8d49182
Andres Fernandez <71739579+andresfernandez89@users.noreply.github.com>
2022-08-31 18:29:09 -0300

    basename root added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter basename="/">
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>
 );
